efd232885cc60a2c4d64ff282ebae3592a257a43
lihong <llh18710891669@126.com>
2019-12-05 13:56:22 +0800

    Update BaogeApp.java

--- app/src/main/java/demo/soho/com/baogevideo/BaogeApp.java
+++ app/src/main/java/demo/soho/com/baogevideo/BaogeApp.java
@@ -52,11 +52,9 @@ public class BaogeApp extends Application {
             @Override
             public void onSuccess(Object data, int flag) {
                 //token在设备卸载重装的时候有可能会变
-                Log.d("TPush", "注册成功，设备token为：" + data);
             }
             @Override
             public void onFail(Object data, int errCode, String msg) {
-                Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
             }
         });
     }
